4dc34597762e6fab405088e6e84ce969b10ac011
krava <kravalex2004@gmail.com>
2024-11-12 15:35:01 +0200

    Sidebar tablet

--- src/assets/imgs/index.js
+++ src/assets/imgs/index.js
@@ -1,7 +1,4 @@
-import regBgSmall from './regBgSmall.png';
-import regBgLarge from './regBgLarge.png';
 import defaultAvatar from './default-avatar.png';
 import avatarWhite from './avatar-white.png';
-import sidebarBg from './sidebarBg.png';
 
-export { regBgLarge, regBgSmall, defaultAvatar, avatarWhite, sidebarBg };
+export { defaultAvatar, avatarWhite };
