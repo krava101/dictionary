1f08ff1a58bfe3a15afe7a1b5e8b56e0abb8e718
krava <kravalex2004@gmail.com>
2024-11-12 15:50:17 +0200

    Header

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -8,10 +8,14 @@ const UserInfo = ({ openMenu }) => {
   };
   return (
     <div className={css.info}>
-      <p>Name</p>
-      <img src={defaultAvatar} alt="avatar" />
+      <p className={css.name}>Name</p>
+      <img className={css.avatar} src={defaultAvatar} alt="avatar" />
       <button className={css.menuBtn} type="button" onClick={handleOpenSidebar}>
         <Icon name="burger" />
+      </button>
+      <button className={css.logOutBtn} type="button">
+        Log out
+        <Icon name="arrow-right" stroke="#121417" />
       </button>
     </div>
   );
